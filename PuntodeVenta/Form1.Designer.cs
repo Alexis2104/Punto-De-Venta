30446c37030b14058ed1e31837876a6169162bad
Alexis2104 <118292269+Alexis2104@users.noreply.github.com>
2023-04-26 22:26:21 -0600

    Add files via upload

---
+++ PuntodeVenta/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace PuntodeVenta
+{
+    partial class frmPOS
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.iconMenuItem7 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem6 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem5 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem4 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem3 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem2 = new FontAwesome.Sharp.IconMenuItem();
+            this.iconMenuItem1 = new FontAwesome.Sharp.IconMenuItem();
+            this.menuTop = new System.Windows.Forms.MenuStrip();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.AutoSize = false;
+            this.menuStrip2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iconMenuItem7,
+            this.iconMenuItem6,
+            this.iconMenuItem5,
+            this.iconMenuItem4,
+            this.iconMenuItem3,
+            this.iconMenuItem2,
+            this.iconMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 100);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(270, 611);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            this.menuStrip2.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
+            // 
+            // iconMenuItem7
+            // 
+            this.iconMenuItem7.AutoSize = false;
+            this.iconMenuItem7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem7.IconChar = FontAwesome.Sharp.IconChar.UserPlus;
+            this.iconMenuItem7.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem7.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem7.Name = "iconMenuItem7";
+            this.iconMenuItem7.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem7.Text = "USUARIOS";
+            // 
+            // iconMenuItem6
+            // 
+            this.iconMenuItem6.AutoSize = false;
+            this.iconMenuItem6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem6.IconChar = FontAwesome.Sharp.IconChar.Key;
+            this.iconMenuItem6.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem6.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem6.Name = "iconMenuItem6";
+            this.iconMenuItem6.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem6.Text = "MANTENIMIENTO";
+            this.iconMenuItem6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconMenuItem5
+            // 
+            this.iconMenuItem5.AutoSize = false;
+            this.iconMenuItem5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem5.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.iconMenuItem5.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem5.Name = "iconMenuItem5";
+            this.iconMenuItem5.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem5.Text = "COMPRAS";
+            this.iconMenuItem5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconMenuItem4
+            // 
+            this.iconMenuItem4.AutoSize = false;
+            this.iconMenuItem4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem4.IconChar = FontAwesome.Sharp.IconChar.Tag;
+            this.iconMenuItem4.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem4.Name = "iconMenuItem4";
+            this.iconMenuItem4.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem4.Text = "CATALOGO";
+            this.iconMenuItem4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconMenuItem3
+            // 
+            this.iconMenuItem3.AutoSize = false;
+            this.iconMenuItem3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem3.IconChar = FontAwesome.Sharp.IconChar.PeopleGroup;
+            this.iconMenuItem3.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem3.Name = "iconMenuItem3";
+            this.iconMenuItem3.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem3.Text = "ADMINISTRADOR";
+            this.iconMenuItem3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconMenuItem2
+            // 
+            this.iconMenuItem2.AutoSize = false;
+            this.iconMenuItem2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconMenuItem2.IconChar = FontAwesome.Sharp.IconChar.IdCard;
+            this.iconMenuItem2.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem2.Name = "iconMenuItem2";
+            this.iconMenuItem2.Size = new System.Drawing.Size(261, 100);
+            this.iconMenuItem2.Text = "IDENTIFIICACION";
+            this.iconMenuItem2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // iconMenuItem1
+            // 
+            this.iconMenuItem1.AutoSize = false;
+            this.iconMenuItem1.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.iconMenuItem1.IconColor = System.Drawing.Color.Black;
+            this.iconMenuItem1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconMenuItem1.Name = "iconMenuItem1";
+            this.iconMenuItem1.Size = new System.Drawing.Size(261, 85);
+            this.iconMenuItem1.Text = "Usuario";
+            this.iconMenuItem1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconMenuItem1.Click += new System.EventHandler(this.iconMenuItem1_Click);
+            // 
+            // menuTop
+            // 
+            this.menuTop.AutoSize = false;
+            this.menuTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(124)))), ((int)(((byte)(204)))));
+            this.menuTop.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuTop.Location = new System.Drawing.Point(0, 0);
+            this.menuTop.Name = "menuTop";
+            this.menuTop.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuTop.Size = new System.Drawing.Size(1300, 100);
+            this.menuTop.TabIndex = 2;
+            this.menuTop.Text = "menuStrip3";
+            this.menuTop.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuTop_ItemClicked);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.lblTitle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitle.Location = new System.Drawing.Point(302, 19);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(781, 70);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "SISTEMAS DE COMPRAS Y VENTAS POS";
+            // 
+            // frmPOS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 711);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmPOS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "POS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuTop;
+        private System.Windows.Forms.Label lblTitle;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem1;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem7;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem6;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem5;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem4;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem3;
+        private FontAwesome.Sharp.IconMenuItem iconMenuItem2;
+    }
+}
+
---
+++ PuntodeVenta/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntodeVenta
+{
+    public partial class frmPOS : Form
+    {
+        public frmPOS()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuTop_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mantenimientoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
